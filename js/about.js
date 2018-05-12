e7d63db6189c06052dfc0080550ad6a814faf49b
Chris0602 <15618628406@163.com>
2018-05-12 17:16:08 +0800

    add shoes

---
+++ js/about.js
@@ -0,0 +1,29 @@
+/**
+ * Created by cyb on 2018/5/10.
+ */
+
+(function(){
+  var $menu = document.getElementById('menu'),
+    $body = document.getElementsByTagName('body')[0],
+    $box = document.getElementById('box'),
+    $img_bask = document.getElementsByClassName('img_bask')[0],
+    $closeBtn = document.getElementById('close_btn'),
+    $img = document.getElementById('img');
+
+  $menu.addEventListener('click', function(){
+    if($box.className.indexOf('show_box') !== -1){
+      $box.className = 'mobild_navi_box';
+    } else {
+      $box.className = 'mobild_navi_box show_box';
+    }
+  });
+  $img.addEventListener('click',function(){
+    $img_bask.style.display = 'block';
+    $body.style.overflow = 'hidden';
+  });
+  $closeBtn.addEventListener('click',function(){
+    $img_bask.style.display = 'none';
+    $body.style.overflow = 'auto';
+  })
+
+})()
